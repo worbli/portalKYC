cc3a544a6c742c3acff555586e41e04768a80d72
Will Hill <email.will.in.china@gmail.com>
2018-11-07 20:13:59 +0800

    rm dist

--- worbli-kyc.js
+++ worbli-kyc.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static('dist'))
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
